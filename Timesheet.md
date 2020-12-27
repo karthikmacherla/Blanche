d6e5aa6cb4dce43dbffffaf390194a9b93a4e64a
Karthik Macherla <kmacher@seas.upenn.edu>
2020-12-27 16:31:12 -0500

    Update styling + centering

--- Timesheet.md
+++ Timesheet.md
@@ -1,3 +1,11 @@
 ## Timesheets (as of 12.27.2020)
 
 - Initial Repo, 1:30pm - 2:00pm
+- Made modal + add remove functionality, 2:10pm - 2:55pm
+- Basic Styling, Trying to focus search bar, 3:00pm - 4:30pm
+
+
+
+## Currently Working On ...
+- How to focus on search bar? Iframe offers isolation, but prevents interaction with inside elements. 
+- May have to just add this to the screen raw? How to add bootstrap css?
--- content.js
+++ content.js
@@ -11,32 +11,49 @@ chrome.runtime.onMessage.addListener((request) => {
 
 /**
  * Creates the spotlight search modal
+ * @relevant "modal-container" is the id of the popup
  */
 function createModal() {
+  // Styling the container holding the iframe itself
   const modal = document.createElement("div");
   modal.setAttribute(
     "style", `
+      padding: 7px;
+      margin: initial;
       height:100px;
       left: 25%;
       right: 25%;
       border: none;
       top:10%;
-      border-radius:7px;
+      -webkit-border-radius:7px;
       background-color:white;
       position: fixed; 
       box-shadow: 0px 12px 48px rgba(29, 5, 64, 0.32);
     `
   );
+  /* TODO: Figure out how to fill the div completely */
   modal.setAttribute("id", "modal-container");
-  modal.innerHTML = `<iframe id="popup-content"; style="height:100%"></iframe>`;
+  modal.innerHTML = `<iframe id="popup-content"; 
+    style="
+      height:100%;
+      width: 100%;
+      "></iframe>`;
+  //add everything to the screen
   document.body.appendChild(modal);
   const iframe = document.getElementById("popup-content");
   iframe.src = chrome.runtime.getURL(modalView);
   iframe.frameBorder = 0;
+
+  /* TODO: Focus search bar on open */
+  var searchBar = iframe.contentWindow.document.getElementById("search-bar");
+
+  iframe.contentWindow.focus();
+  searchBar.focus();
 }
 
 /**
  * Destroys the spotlight search model if it exists
+ * @relevant uses modal-container as the id
  */
 function destroyModal() {
   console.log("Destroying modal");
